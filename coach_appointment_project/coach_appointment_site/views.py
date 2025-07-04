bd7d265dd8a276bafff74ed5d5205b818848cf54
CpHeat <charles.petit.59@gmail.com>
2025-07-04 12:01:21 +0200

    implemented user groups

--- coach_appointment_project/coach_appointment_site/views.py
+++ coach_appointment_project/coach_appointment_site/views.py
@@ -1,20 +1,31 @@
 from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import Group
 from django.shortcuts import render, redirect
 
 @login_required(login_url="/login/")
-def index(request):
+def index_view(request):
     return render(request, "coach_appointment_site/index.html", {})
+
+@login_required(login_url="/login/")
+def dashboard_view(request):
+    return render(request, "coach_appointment_site/dashboard.html", {})
 
 def signup_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            # Saves the user and attach it to client group
+            user = form.save()
+            client_group, created = Group.objects.get_or_create(name='client')
+            user.groups.add(client_group)
+
+            # Logs the user
             new_user = authenticate(username=form.cleaned_data['username'],
                                     password=form.cleaned_data['password1'],)
             login(request, new_user)
+
             return redirect("coach_appointment_site:login")
     else:
         form = UserCreationForm()
